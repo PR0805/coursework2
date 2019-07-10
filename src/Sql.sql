c5ffff3a541217dc2c3f2cef7c2655a5ce6f5070
cce2050 <cce2050@localhost.localdomain>
2019-07-10 19:16:30 +0100

    commit

--- src/Sql.sql
+++ src/Sql.sql
@@ -18,9 +18,19 @@ CREATE TABLE Showings(
 	movie_title VARCHAR(50) NOT NULL,
 	date_time DATETIME NOT NULL,
         room_number INTEGER NOT NULL,
+	tickets_available INTEGER NOT NULL,
 	PRIMARY KEY(showing_id),
         FOREIGN KEY(movie_title) REFERENCES Films(movie_title)
 );
+
+CREATE TABLE TicketSales(
+	sales_id INTEGER NOT NULL AUTO_INCREMENT, 
+	showing_id INTEGER NOT NULL,
+        quantity INTEGER NOT NULL,
+	PRIMARY KEY(sales_id),
+        FOREIGN KEY(showing_id) REFERENCES Showings(showing_id)
+);
+
 
 
 
@@ -30,9 +40,13 @@ VALUES ('Aquaman', 'Jason Momoa', 'James Wan Peldi', 'Fantasy', 'PG-13'),
 ('Bumblebee', 'Hailee Steinfield', 'Travis Knight', 'Sci-Fic', 'PG-13');
 
 INSERT INTO Showings 
-VALUES ('1', 'Aquaman', '19/03/01 10:00', '10'),
-('2', 'Mortal Engines', '19/03/01 10:00', '11'),
-('3', 'Bumblebee', '19/03/01 10:00', '12');
+VALUES ('1', 'Aquaman', '19/03/01 10:00', '10', '30'),
+('2', 'Mortal Engines', '19/03/01 10:00', '11', '20'),
+('3', 'Bumblebee', '19/03/01 10:00', '12','25');
+
+INSERT INTO TicketSales (showing_id, quantity)
+VALUES ('1', '1');
+
 
 
 
